0a63f19f10e93c5c6b1b079f9e5074967758e863
khanmohammadimran <khanmohammadimran.i31@gmail.com>
2022-08-21 17:08:03 +0600

    Filter sections reducer created

---
+++ src/redux/Filter/Reducer.js
@@ -0,0 +1,36 @@
+import { COLORCHANGED, STATUSCHANGED } from "./ActionTypes";
+import initialState from "./InitialState";
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case STATUSCHANGED:
+      return {
+        ...state,
+        status: action.payload,
+      };
+    case COLORCHANGED:
+      const { color, changeType } = action.payload;
+      switch (changeType) {
+        case "added":
+          return {
+            ...state,
+            colors: [...state.colors, color],
+          };
+        case "removed":
+          return {
+            ...state,
+            colors: state.colors.filter(
+              (existingColor) => existingColor !== color
+            ),
+          };
+
+        default:
+          return state;
+      }
+
+    default:
+      return state;
+  }
+};
+
+export default reducer;
